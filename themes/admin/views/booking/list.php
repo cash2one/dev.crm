5b3a5ca557107a5ab693764265e3c10a6d4adf53
zhangks <1307716475@qq.com>
2016-02-04 13:48:01 +0800

    list改为表格

--- themes/admin/views/booking/list.php
+++ themes/admin/views/booking/list.php
@@ -13,11 +13,28 @@ $this->menu = array(
 ?>
 
 <h1>预约列表</h1>
-
-<?php
-$this->widget('zii.widgets.CListView', array(
-    'dataProvider' => $dataProvider,
-    'itemView' => '_viewList',
-));
-?>
-<style>.list-view{position:relative;}</style>
+<div class="data-list mt10">
+    <table class="table">
+        <thead>
+            <tr>
+                <td>ID</td>
+                <td>预约号</td>
+                <td>所约专家</td>
+                <td>手机号</td>
+                <td>患者姓名</td>
+                <td>状态</td>
+                <td>创建日期</td>
+                <td>病历数量</td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $this->widget('zii.widgets.CListView', array(
+                'dataProvider' => $dataProvider,
+                'itemView' => '_viewList',
+            ));
+            ?>
+        </tbody>
+    </table>
+</div>
+<style>.data-list,.list-view{position:relative;}.pager{margin: 0;}</style>
